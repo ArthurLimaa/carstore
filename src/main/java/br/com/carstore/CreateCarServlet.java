1669888662f95ceb8353b9c95fa39de70a35ae5a
Arthur <arthur.ondichiatti03@gmail.com>
2024-03-27 21:54:47 -0300

    subindo arquivos

--- src/main/java/br/com/carstore/CreateCarServlet.java
+++ src/main/java/br/com/carstore/CreateCarServlet.java
@@ -1,5 +1,7 @@
 package br.com.carstore;
 
+import br.com.carstore.dao.CarDAO;
+import br.com.carstore.model.Car;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,9 +15,16 @@ public class CreateCarServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        Car car = new Car();
+
+
         String carName = request.getParameter("carName");
 
-        System.out.println(carName);
+        car.setName(carName);
+
+        CarDAO dao = new CarDAO();
+
+        dao.createCar(car);
 
         request.getRequestDispatcher("index.html").forward(request, response);
 
