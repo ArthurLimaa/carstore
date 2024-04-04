57a336a43feb020d358db4730299d5fe189072b3
Arthur <arthur.ondichiatti03@gmail.com>
2024-04-03 22:25:43 -0300

    subindo arquivos

--- src/main/java/br/com/carstore/CreateCarServlet.java
+++ src/main/java/br/com/carstore/CreateCarServlet.java
@@ -2,6 +2,7 @@ package br.com.carstore;
 
 import br.com.carstore.dao.CarDAO;
 import br.com.carstore.model.Car;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
--- src/main/java/br/com/carstore/dao/CarDAO.java
+++ src/main/java/br/com/carstore/dao/CarDAO.java
@@ -5,10 +5,17 @@ import br.com.carstore.model.Car;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
 
 public class CarDAO {
 
     public void createCar(Car car) {
+
 
         String SQL = "INSERT INTO CAR (NAME) VALUES (?)";
 
@@ -21,7 +28,6 @@ public class CarDAO {
             PreparedStatement preparedStatement = connection.prepareStatement(SQL);
 
             preparedStatement.setString(1, car.getName());
-
             preparedStatement.execute();
 
             System.out.println("success in insert car");
@@ -30,10 +36,53 @@ public class CarDAO {
 
         } catch (Exception e) {
 
-            System.out.println("fail in database connection");
+            System.out.println("falha ao cadastrar os carros de DB");
+
+
 
         }
-
     }
 
+
+    public List<Car> findAllCars() {
+
+        String SQL = "SELECT * FROM car";
+
+        try {
+
+            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa");
+
+            System.out.println("success in database connection");
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<Car> cars = new ArrayList<>();
+
+            while (resultSet.next()) {
+
+                String carName = resultSet.getString("name");
+
+                Car car = new Car();
+                car.setName(carName);
+
+                cars.add(car);
+
+            }
+
+            System.out.println("success in select * car");
+
+            connection.close();
+
+            return cars;
+
+        } catch (Exception e) {
+
+            System.out.println("fail in database connection");
+
+            return Collections.emptyList();
+
+        }
+    }
 }
